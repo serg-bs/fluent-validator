c36bd21f731860b2163411d3c78995626ff67c4d
Беспалов Сергей Михайлович <bespalov@srvhub.ru>
2019-05-14 12:52:35 +0300

    addResult to ComplexResult for gathering result

--- fluent-validator/src/main/java/com/baidu/unbiz/fluentvalidator/ComplexResult.java
+++ fluent-validator/src/main/java/com/baidu/unbiz/fluentvalidator/ComplexResult.java
@@ -1,5 +1,9 @@
 package com.baidu.unbiz.fluentvalidator;
 
+import com.baidu.unbiz.fluentvalidator.util.CollectionUtil;
+
+import java.util.ArrayList;
+
 /**
  * 带有全信息的复杂验证结果
  *
@@ -21,6 +25,20 @@ public class ComplexResult extends GenericResult<ValidationError> {
 
     public void setTimeElapsed(int timeElapsed) {
         this.timeElapsed = timeElapsed;
+    }
+
+    public void addResult(ComplexResult complexResult){
+        if(!complexResult.isSuccess()){
+            this.setIsSuccess(false);
+        }
+        if(!CollectionUtil.isEmpty(complexResult.getErrors())){
+            if (CollectionUtil.isEmpty(this.getErrors())){
+                this.setErrors(new ArrayList<ValidationError>());
+            }
+
+            this.getErrors().addAll(complexResult.getErrors());
+        }
+
     }
 
 }
